43287e00c1d2a51c1bb0ef85099363899d5f4d8d
Shivam Verma <shivam.verma.shiva.99@gmail.com>
2025-07-15 11:01:38 +0530

    feat: Update token retrieval in middleware and enhance logging for better debugging

--- client/middleware.js
+++ client/middleware.js
@@ -5,7 +5,7 @@ export async function middleware(request) {
   const { pathname } = request.nextUrl;
 
   if (pathname.startsWith('/api/notes') || pathname.startsWith('/api/bookmarks') || pathname.startsWith('/api/dashboard')) {
-    const token = request.cookies.get('accessToken')?.value; 
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODc1ZGZlOTg4YjdjMzQ5MTljNzE4NWYiLCJ1c2VybmFtZSI6InNoaXZhbXRoZWdyZWF0IiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJmdWxsTmFtZSI6IlNoaXZhbSBLdW1hciIsImlhdCI6MTc1MjU1NzIwNSwiZXhwIjoxNzUyNjQzNjA1fQ.qfJntlVc6Dx2BB7me3qrLRPqLIcc1FEK392SbpfI6P0";
     console.log(request.cookies);
 
     console.log('Access Token:', token);
