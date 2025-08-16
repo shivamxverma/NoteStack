8a2ba5ddedf265a48673d8e54d091a2506b596e3
Shivam Verma <shivam.verma.shiva.99@gmail.com>
2025-08-17 00:39:18 +0530

    feat: Add initial Tailwind CSS configuration file

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "media", // or "class" if you want manual dark mode toggle
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
